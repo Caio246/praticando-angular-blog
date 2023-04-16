67ba3c1f93c8848116a2a2eb5cb0fe84deba0ca2
Caio246 <caiostun@gmail.com>
2023-04-16 14:16:31 -0300

    Personizações do blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Rewards",
+    "description": "Zona de testes",
+    "photoCover":"//d3b4yo2b5lbfy.cloudfront.net/wp-content/uploads/2023/03/e4566GW2-SABweapons2023-1920x1080-1.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Super Adventure Festival 2023 Begins Today",
+    "description": "by The Guild Wars 2 Team on March 28, 2023",
+    "photoCover":"//d3b4yo2b5lbfy.cloudfront.net/wp-content/uploads/2023/03/55dd2SAB_Environment.08.jpg"
   }
 ]
